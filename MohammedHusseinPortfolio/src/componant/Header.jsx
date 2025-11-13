ee66d62edc62e96687ea0dc1fad972d78d2a9a96
ENG. Mohammed Hussein kamel <108235027+WLMEY@users.noreply.github.com>
2025-11-13 06:17:01 +0200

    stop hrer

--- MohammedHusseinPortfolio/src/componant/Header.jsx
+++ MohammedHusseinPortfolio/src/componant/Header.jsx
@@ -1,23 +1,23 @@
 const Header = () => {
-    return (  <div className="Header  hidden sm:flex  flex-row justify-between items-center  p-5 rounded-2xl gap-10">
+    return (  <div className="Header  hidden sm:flex  flex-row justify-between items-center  sm:p-5  rounded-2xl gap-10">
         <div className="icons flex flex-row justify-between items-center gap-3">
-            <i className="fa-solid fa-at"></i>
-            <i className="fa-solid fa-code"></i>
-            <i className="fa-brands fa-linkedin-in"></i>
+            <a href="mailto:dev.mohammedhussein@gmail.com" target="_blank"> <i className=" fa-solid fa-at"></i>          </a>         
+            <a href="https://github.com/WLMEY?tab=overview&from=2025-01-01&to=2025-01-01" target="_blank"> <i className="fa-solid fa-code"></i>         </a>       
+            <a href="https://www.linkedin.com/in/mohammed-hussein-kamel-4247b4211/" target="_blank"> <i className="fa-brands fa-linkedin-in"></i> </a>      
         </div>
 
         <div className="titles flex flex-row justify-between items-center w-1/3">
-            <span>Home</span>
-            <span>Project</span>
-            <span>Skills</span>
-            <span>Contact</span>
+            <span>   <a className=" icon" href="#Home"   >Home</a></span>
+            <span>   <a className=" icon" href="#Project">Project</a></span>
+            <span>   <a className=" icon" href="#Skills" >Skills </a></span>
+            <span>   <a className=" icon" href="#Contact">Contact</a></span>
 
 
         </div>
 
         <div className="loco flex flex-row justify-between items-center gap-3">
             <span>Mohammad Hussein</span>
-            <i class=" logoCode fa-solid fa-code"></i>
+            <i className=" logoCode fa-solid fa-code"></i>
 
 
 
--- MohammedHusseinPortfolio/src/componant/home.jsx
+++ MohammedHusseinPortfolio/src/componant/home.jsx
@@ -1,115 +1,296 @@
 import Header from "./Header";
 import myimage from '../assets/mohammed hussein.jpeg'
+import webpage from '../assets/webpage test1.png'
+
 const Home = () => {
-    return (<div className="Main sm:p-30 p-5 flex flex-col gap-40">
+    const aboutme = "Creating modern and responsive web sexperiences.I bild beautiful, functional, and user-friendly websites. and application"
+    return (<div className="Main sm:p-30 p-5 pt-15 flex flex-col gap-40">
 
         <Header />
 
-        <div className="Hero flex sm:flex-row flex-col  sm:items-start items-center sm:justify-between justify-center sm:gap-0 gap-4">
+        <div className="Hero flex sm:flex-row flex-col  sm:items-start items-center sm:justify-between justify-center sm:gap-0 gap-10">
             <div className="image sm:w-1/2 rounded-b-full  flex items-center sm:justify-start justify-center">
                 <img src={myimage} className="max-w-1/2 rounded-full" />
             </div>
 
-            <div className="Aboutme sm:w-1/2 flex flex-col sm:items-start items-center text-center sm:gap-0 gap-4">
-                <h1 className="block sm:hidden  mainText text-5xl">Mohammed Hussein Kamel</h1>
-                <h1 className=" hidden sm:block job mainText text-7xl">Front-End Web Developer</h1>
-                <p className="subText " >Creating modern and responsive web sexperiences.
-                    I bild beautiful, functional, and user-friendly websites. and application
-
-                </p>
+            <div className="Aboutme sm:w-1/2 flex flex-col sm:items-start  items-center sm:text-start text-center sm:gap-7 gap-10">
+                <h1 className="block sm:hidden  mainText text-3xl">Mohammed Hussein Kamel</h1>
+                <h1 className=" hidden sm:block job mainText text-7xl ">Front-End Web Developer</h1>
+                <p className="subText  hidden sm:block" >{aboutme}</p>
                 <div className="btns">
-                    <button>View my Work</button>
-                    <div className="btn_phone  flex flex-row justify-between" >
-                        <a className="btn" href="https://github.com/WLMEY?tab=overview&from=2025-01-01&to=2025-01-01" target="_blank">GitHub</a>
-                        <a className="btn" href="https://www.linkedin.com/in/mohammed-hussein-kamel-4247b4211/" target="_blank">Linkedin</a>
+                    <button className=" btn_wiewmywork hidden sm:block text-white pt-2 pb-2 pl-20 pr-20 rounded-sm">View my Work</button>
+                    <div className="btn_phone  sm:hidden flex flex-row justify-between w-full items-center gap-5" >
+                        <a className="btn btn_1  text-3xl  text-white pt-2 pb-2 pl-15 pr-15 rounded-xl " href="https://github.com/WLMEY?tab=overview&from=2025-01-01&to=2025-01-01" target="_blank">GitHub</a>
+                        <a className="btn        text-3xl  text-white  pt-2 pb-2 pl-15 pr-15 rounded-xl " href="https://www.linkedin.com/in/mohammed-hussein-kamel-4247b4211/" target="_blank">Linkedin</a>
 
                     </div>
                 </div>
             </div>
+            <div className="sm:hidden flex flex-col justify-center gap-3 items-start">
+                <h2 className="text-3xl">About Me</h2>
+                <p className="subText " >{aboutme}</p>
+            </div>
         </div>
 
-<div>
-        <h1 className="text-5xl">My Projects</h1>
-        <div className="Myprojects flex flex-row justify-between items-center  gap-6 "  >
+        <div>
+            <h1 className="text-5xl mb-10">My Projects</h1>
+            <div className="Myprojects flex sm:flex-row flex-col justify-between items-center  gap-6 "  >
 
 
 
-            <div className="project flex flex-col justify-center items-center p-5 rounded-2xl " >
-                <div className="project_img ">
-                    <img src={myimage} className="rounded-2xl " alt="" />
+                <div className="project flex flex-col justify-center items-center p-5 rounded-2xl " >
+                    <div className="project_img ">
+                        <img src={webpage} className="rounded-2xl " alt="" />
+                    </div>
+                    <div className="project_about  flex flex-col justify-center items-start sm:gap-0 gap-2" >
+                        <h3 className="mainText text-3xl ">title</h3>
+                        <p className="subText">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi, magni.</p>
+                        <div className="project_tecnology  placeholders flex flex-row justify-start items-center gap-3">
+                            <span className="project_skills w-15 sm:w-fit pt-0.5 sm:pt-0 pb-0.5  sm:pb-0 rounded-2xl  text-sm text-center">vue.js</span>
+                            <span className="project_skills w-15 sm:w-fit pt-0.5 sm:pt-0 pb-0.5  sm:pb-0 rounded-2xl  text-sm text-center" >Css</span>
+                            <span className="project_skills w-15 sm:w-fit pt-0.5 sm:pt-0 pb-0.5  sm:pb-0 rounded-2xl  text-sm text-center" >Api</span>
+                        </div>
+                        <span className="btn block sm:hidden  w-full p-2 text-center rounded-2xl mt-5"> View Live</span>
+                    </div>
                 </div>
-                <div className="project_about  flex flex-col justify-center items-start" >
-                    <h3 className="mainText text-3xl ">title</h3>
-                    <p className="subText">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi, magni.</p>
-                    <p className="project_tecnology  placeholders"> vue.js, Css, api</p>
+
+
+                <div className="project overflow-hidden flex flex-col justify-center items-center sm:p-5 rounded-2xl " >
+                    <div className="project_img ">
+                        <img src={webpage} className=" " alt="" />
+                    </div>
+                    <div className="project_about  flex flex-col justify-center items-start p-5 sm:p-0  sm:gap-0 gap-2" >
+                        <h3 className="mainText text-3xl ">title</h3>
+                        <p className="subText">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi, magni.</p>
+                        <div className="project_tecnology  placeholders flex flex-row justify-start items-center gap-3">
+                            <span className="project_skills w-15 sm:w-fit pt-0.5 sm:pt-0 pb-0.5  sm:pb-0 rounded-2xl  text-sm text-center">vue.js</span>
+                            <span className="project_skills w-15 sm:w-fit pt-0.5 sm:pt-0 pb-0.5  sm:pb-0 rounded-2xl  text-sm text-center" >Css</span>
+                            <span className="project_skills w-15 sm:w-fit pt-0.5 sm:pt-0 pb-0.5  sm:pb-0 rounded-2xl  text-sm text-center" >Api</span>
+                        </div>
+                        <span className="btn block sm:hidden  w-full p-2 text-center rounded-2xl mt-5"> View Live</span>
+                    </div>
                 </div>
+
+                <div className="project flex flex-col justify-center items-center p-5 rounded-2xl " >
+                    <div className="project_img ">
+                        <img src={webpage} className="rounded-2xl " alt="" />
+                    </div>
+                    <div className="project_about  flex flex-col justify-center items-start" >
+                        <h3 className="mainText text-3xl ">title</h3>
+                        <p className="subText">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi, magni.</p>
+                        <p className="project_tecnology  placeholdersText"> vue.js, Css, api</p>
+                    </div>
+                </div>
+
+
+
+
+            </div>
+        </div>
+
+
+        <div>
+            <h1 className="text-5xl">My Skills</h1>
+            <div className="MySkills hidden sm:flex flex-row justify-between items-center  gap-6  flex-wrap w-full pt-10">
+
+                <div className="skill flex flex-col justify-between items-center p-3 w-26 rounded-2xl gap-3"     >
+                    <img src={myimage} className="w-full rounded-2xl " alt="" />
+                    <h1> React</h1>
+                </div><div className="skill flex flex-col justify-between items-center p-3 w-26 rounded-2xl gap-3 " >
+                    <img src={myimage} className="w-full rounded-2xl " alt="" />
+                    <h1> Git</h1>
+                </div><div className="skill flex flex-col justify-between items-center p-3 w-26 rounded-2xl gap-3" >
+                    <img src={myimage} className="w-full rounded-2xl " alt="" />
+                    <h1> JavaScript</h1>
+                </div><div className="skill flex flex-col justify-between items-center p-3 w-26 rounded-2xl gap-3" >
+                    <img src={myimage} className="w-full rounded-2xl " alt="" />
+                    <h1> TailwindCSS</h1>
+                </div><div className="skill flex flex-col justify-between items-center p-3 w-26 rounded-2xl gap-3" >
+                    <img src={myimage} className="w-full rounded-2xl " alt="" />
+                    <h1> F Motion </h1>
+                </div><div className="skill flex flex-col justify-between items-center p-3 w-26 rounded-2xl gap-3" >
+                    <img src={myimage} className="w-full rounded-2xl " alt="" />
+                    <h1> Redux</h1>
+                </div><div className="skill flex flex-col justify-between items-center p-3 w-26 rounded-2xl gap-3" >
+                    <img src={myimage} className="w-full rounded-2xl " alt="" />
+                    <h1> HTML5</h1>
+                </div><div className="skill flex flex-col justify-between items-center p-3 w-26 rounded-2xl gap-3" >
+                    <img src={myimage} className="w-full rounded-2xl " alt="" />
+                    <h1> CSS3</h1>
+                </div><div className="skill flex flex-col justify-between items-center p-3 w-26 rounded-2xl gap-3" >
+                    <img src={myimage} className="w-full rounded-2xl " alt="" />
+                    <h1> Axios</h1>
+                </div>
+
             </div>
 
 
-            <div className="project flex flex-col justify-center items-center p-5 rounded-2xl " >
-                <div className="project_img ">
-                    <img src={myimage} className="rounded-2xl " alt="" />
+
+            <div className="myskills_phone flex sm:hidden flex-col gap-5  justify-center items-start">
+                <h2 className="text-3xl mt-5">Core Languages</h2>
+                <div className="skillGrops grid grid-cols-2 w-full gap-3  p-2">
+                    <div className="skill flex flex-col justify-between items-center p-3   rounded-2xl gap-3" >
+                        <h4 className=" text-3xl">JS</h4>
+                        <div>
+                            <h1> JavaScript (ES6+)</h1>
+                            <p>good</p>
+
+                        </div>
+                    </div>
+                    <div className="skill flex flex-col justify-between items-center p-3   rounded-2xl gap-3" >
+                        <i class="text-3xl fa-brands fa-html5"></i>
+                        <div>
+                            <h1>HTML5</h1>
+                            <p>good</p>
+
+                        </div>
+                    </div>
+                    <div className="skill flex flex-col justify-between items-center p-3   rounded-2xl gap-3" >
+                        <i class="text-3xl fa-brands fa-css3-alt"></i>
+                        <div>
+                            <h1> CSS3</h1>
+                            <p>good</p>
+
+                        </div>
+                    </div>
                 </div>
-                <div className="project_about  flex flex-col justify-center items-start" >
-                    <h3 className="mainText text-3xl ">title</h3>
-                    <p className="subText">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi, magni.</p>
-                    <p className="project_tecnology  placeholders"> vue.js, Css, api</p>
+
+
+
+
+
+
+                <h2 className="text-3xl mt-5">Frameworks & Libraries</h2>
+                <div className="skillGrops grid grid-cols-2 w-full gap-3  p-2">
+                    <div className="skill flex flex-col justify-between items-center p-3   rounded-2xl gap-3" >
+                        {/* <h4 className=" text-3xl">JS</h4> */}
+                        <i className=" text-3xl fa-brands fa-react"></i>
+                        <div>
+                            <h1> React</h1>
+                            <p>good</p>
+
+                        </div>
+                    </div>
+                    <div className="skill flex flex-col justify-between items-center p-3   rounded-2xl gap-3" >
+                        {/* <i class=" fa-brands fa-html5"></i> */}
+                        <i class= "text-3xl fa-solid fa-sitemap"></i>
+                        <div>
+                            <h1>Redux</h1>
+                            <p>good</p>
+
+                        </div>
+                    </div>
+                    <div className="skill flex flex-col justify-between items-center p-3   rounded-2xl gap-3" >
+                        {/* <i class="text-3xl fa-brands fa-css3-alt"></i> */}
+                        <h4 className=" text-3xl">API</h4>
+
+                        <div>
+                            <h1> Axios</h1>
+                            <p>good</p>
+
+                        </div>
+                    </div>
+
+
+                    <div className="skill flex flex-col justify-between items-center p-3   rounded-2xl gap-3" >
+                        <i class="text-3xl fa-brands fa-css3-alt"></i>
+                        <div>
+                            <h1> TailwindCSS</h1>
+                            <p>good</p>
+
+                        </div>
+                    </div>
+                    <div className="skill flex flex-col justify-between items-center p-3   rounded-2xl gap-3" >
+                        {/* <i class="text-3xl fa-brands fa-css3-alt"></i> */}
+                        <i class="text-3xl fa-solid fa-gears"></i>
+                        <div>
+                            <h1>Framer Motion</h1>
+                            <p>good</p>
+
+                        </div>
+                    </div>
+
+
+
+
+
+
+
+
                 </div>
+
+
+
+
+
+
+                <h2 className="text-3xl mt-5">Tools & Platforms</h2>
+                <div className="skillGrops grid grid-cols-2 w-full gap-3  p-2">
+                    <div className="skill flex flex-col justify-between items-center p-3   rounded-2xl gap-3" >
+                        <h4 className=" text-3xl">Get</h4>
+                        <div>
+                            <h1> Git</h1>
+                            <p>good</p>
+
+                        </div>
+                    </div>
+
+                                        <div className="skill flex flex-col justify-between items-center p-3   rounded-2xl gap-3" >
+                        <h4 className=" text-3xl">Vit</h4>
+                        <div>
+                            <h1> vit</h1>
+                            <p>good</p>
+
+                        </div>
+                    </div>
+                    <div className="skill flex flex-col justify-between items-center p-3   rounded-2xl gap-3" >
+                        {/* <i class="text-3xl fa-brands fa-html5"></i> */}
+                        <i class=" text-3xl fa-solid fa-address-card"></i>
+                        <div>
+                            <h1>Postman</h1>
+                            <p>good</p>
+
+                        </div>
+                    </div>
+                    <div className="skill flex flex-col justify-between items-center p-3   rounded-2xl gap-3" >
+                        {/* <i class=" fa-brands fa-css3-alt"></i> */}
+                        <i class=" text-3xl fa-brands fa-figma"></i>
+                        <div>
+                            <h1> fagma</h1>
+                            <p>good</p>
+
+                        </div>
+
+                    </div>
+                                        <div className="skill flex flex-col justify-between items-center p-3   rounded-2xl gap-3" >
+                        {/* <i class="text-3xl fa-brands fa-css3-alt"></i> */}
+                        <h4 className=" text-3xl">Vercel</h4>
+                        <div>
+                            <h1> Vercel</h1>
+                            <p>good</p>
+
+                        </div>
+
+                    </div>
+
+
+                </div>
+
+
+
+
+
+
+
+
+
+
             </div>
 
-            <div className="project flex flex-col justify-center items-center p-5 rounded-2xl " >
-                <div className="project_img ">
-                    <img src={myimage} className="rounded-2xl " alt="" />
-                </div>
-                <div className="project_about  flex flex-col justify-center items-start" >
-                    <h3 className="mainText text-3xl ">title</h3>
-                    <p className="subText">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi, magni.</p>
-                    <p className="project_tecnology  placeholdersText"> vue.js, Css, api</p>
-                </div>
-            </div>
 
-
-
-
+    <h2 className="btn p-3 rounded-lg w-full text-center mt-10 text-3xl"><i class="fa-solid fa-download"></i> Download CV</h2>
         </div>
-</div>
 
-
-<div>
-        <h1 className="text-5xl">My Skills</h1>
-        <div className="MySkills flex flex-row justify-between items-center  gap-6  flex-wrap w-full pt-10">
-
-            <div className="skill flex flex-col justify-between items-center p-3 w-26 rounded-2xl gap-3"     >
-                <img src={myimage} className="w-full rounded-2xl " alt="" />
-                <h1> React</h1>
-            </div><div className="skill flex flex-col justify-between items-center p-3 w-26 rounded-2xl gap-3 " >
-                <img src={myimage} className="w-full rounded-2xl " alt="" />
-                <h1> Git</h1>
-            </div><div className="skill flex flex-col justify-between items-center p-3 w-26 rounded-2xl gap-3" >
-                <img src={myimage} className="w-full rounded-2xl " alt="" />
-                <h1> JavaScript</h1>
-            </div><div className="skill flex flex-col justify-between items-center p-3 w-26 rounded-2xl gap-3" >
-                <img src={myimage} className="w-full rounded-2xl " alt="" />
-                <h1> TailwindCSS</h1>
-            </div><div className="skill flex flex-col justify-between items-center p-3 w-26 rounded-2xl gap-3" >
-                <img src={myimage} className="w-full rounded-2xl " alt="" />
-                <h1> F Motion </h1>
-            </div><div className="skill flex flex-col justify-between items-center p-3 w-26 rounded-2xl gap-3" >
-                <img src={myimage} className="w-full rounded-2xl " alt="" />
-                <h1> Redux</h1>
-            </div><div className="skill flex flex-col justify-between items-center p-3 w-26 rounded-2xl gap-3" >
-                <img src={myimage} className="w-full rounded-2xl " alt="" />
-                <h1> HTML5</h1>
-            </div><div className="skill flex flex-col justify-between items-center p-3 w-26 rounded-2xl gap-3" >
-                <img src={myimage} className="w-full rounded-2xl " alt="" />
-                <h1> CSS3</h1>
-            </div><div className="skill flex flex-col justify-between items-center p-3 w-26 rounded-2xl gap-3" >
-                <img src={myimage} className="w-full rounded-2xl " alt="" />
-                <h1> Axios</h1>
-            </div>
-
-        </div>
-        </div>
 
         <div className="GetInTouch">
 
